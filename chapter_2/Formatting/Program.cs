6257a2950fe563b0354366bf095aef5437849ab1
caylemh <caylem@bdp-enterprises.co.za>
2020-09-10 20:14:59 +0200

    Working with Inputs (ReadLine)

--- chapter_2/Formatting/Program.cs
+++ chapter_2/Formatting/Program.cs
@@ -22,6 +22,44 @@ namespace Formatting
 
             // WriteToFile(formatted); // writes the string into a file
             Console.WriteLine(formatted);
+
+            // Using interpolated strings
+
+            string applesText = "Apples";
+            int applesCount = 1234;
+            string bananasText = "Bananas";
+            int bananasCount = 56789;
+
+            Console.WriteLine(
+                format: "{0,-8} {1, 6:N0}",
+                arg0: "Name",
+                arg1: "Count"
+            );
+
+            Console.WriteLine(
+                format: "{0,-8} {1, 6:N0}",
+                arg0: applesText,
+                arg1: applesCount
+            );
+
+            Console.WriteLine(
+                format: "{0,-8} {1, 6:N0}",
+                arg0: bananasText,
+                arg1: bananasCount
+            );
+
+            // getting text input
+            Console.Write("\n\nType your firstname and press ENTER: ");
+            string firstname = Console.ReadLine();
+
+            Console.Write("Type your age and press ENTER: ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(
+                format: "Welcome {0} you are {1} years old.",
+                arg0: firstname,
+                arg1: age-7
+            );
         }
     }
 }
