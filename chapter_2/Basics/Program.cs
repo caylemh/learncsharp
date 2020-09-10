a4c46f8be49b52f13742a45ab81c0b7b8d40503a
caylemh <caylem@bdp-enterprises.co.za>
2020-09-10 15:56:49 +0200

    Basics of C#

---
+++ chapter_2/Basics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Temperature on {0:D} is {1} C.", DateTime.Today, 23.4);
+        }
+    }
+}
