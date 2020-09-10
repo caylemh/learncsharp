7f2e706cc597faedfafdffbec8870cba28d7e484
caylemh <caylem@bdp-enterprises.co.za>
2020-09-10 16:46:17 +0200

    Working with Numbers

---
+++ chapter_2/Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // unsigned integer means positive whpole numbers
+            // including 0
+            uint naturalNumber = 23;
+
+            // integer means negative or positive whole numbers
+            // including 0
+            int integerNumber = -23;
+
+            // flost means single-precision floating point
+            // F suffix makes it a float literal
+            float realNumber = 2.3F;
+
+            // double means double-precision floating point
+            double anotherRealNumber = 2.3; // double literal
+
+            // three variables that store the number 2 million
+            int decimalNotation = 2_000_000;
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
+
+            // check the three variables have the same value
+            Console.WriteLine($"{decimalNotation == binaryNotation}");
+            Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+        }
+    }
+}
