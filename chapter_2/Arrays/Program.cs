16af9c446e69294783acaa5b4dfd16ba169da381
caylemh <caylem@bdp-enterprises.co.za>
2020-09-10 19:14:28 +0200

    Working with Arrays

---
+++ chapter_2/Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring an array of string values
+            string[] names = new string[4];
+
+            // storing items at index positions
+            names[0] = "Kate";
+            names[1] = "Jack";
+            names[2] = "Rebecca";
+            names[3] = "Tom";
+
+            // looping through each entry in the array
+            foreach(string name in names) {
+                Console.WriteLine($"Index {Array.IndexOf(names, name) +1} = {name}");
+            }
+        }
+    }
+}
